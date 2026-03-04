e22fdd70feff46e487ada2601639b1aa24a7e261
regenerik <regenerik@gmail.com>
2026-03-03 21:43:34 -0300

    change number

--- client/lib/whatsapp.ts
+++ client/lib/whatsapp.ts
@@ -3,7 +3,7 @@ import { cartStore } from "./flux/CartStore";
 export const getWhatsAppLink = () => {
   const items = cartStore.getItems();
   const total = cartStore.getTotal();
-  const phoneNumber = "5491161901310";
+  const phoneNumber = "5491132283318";
   
   if (items.length === 0) return `https://wa.me/${phoneNumber}?text=${encodeURIComponent("Hola! Quiero hacer un pedido.")}`;
   
