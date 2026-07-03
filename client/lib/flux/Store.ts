9f04acd693cb97befc9819c5000ca765115c86f8
regenerik <regenerik@gmail.com>
2026-07-02 23:33:24 -0300

    precio picada 20000

--- client/lib/flux/Store.ts
+++ client/lib/flux/Store.ts
@@ -156,7 +156,7 @@
       id: "picada-runa",
       name: "Picada Runa",
       description: "Nuggets de pollo x6, Aros de cebolla x6, Bocaditos de espinaca y queso x6, Bastones de muzzarella x6 y Doble porción de papas sazonadas.",
-      price: 18000,
+      price: 20000,
       image: picadaGeboImg,
       isPopular: false,
     },
